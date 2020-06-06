a8a4aa6b78bcb7663d4ecee533309b0297cccef7
Christian Clausen <maxclausen@gmail.com>
2020-06-06 17:44:58 +0200

    Delete setTile

--- index.ts
+++ index.ts
@@ -262,9 +262,6 @@ class Map {
     this.map[y + 1][x] = tile;
     this.map[y][x] = new Air();
   }
-  setTile(x: number, y: number, tile: Tile) {
-    this.map[y][x] = tile;
-  }
   movePlayer(x: number, y: number, newx: number, newy: number) {
     this.map[y][x] = new Air();
     this.map[newy][newx] = new PlayerTile();
