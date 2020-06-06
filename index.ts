5a8ddc115ef31c2b245b98cd23b50a134dda21ec
Christian Clausen <maxclausen@gmail.com>
2020-06-06 16:47:58 +0200

    Push drawMap into Map

--- index.ts
+++ index.ts
@@ -256,6 +256,13 @@ class Map {
       }
     }
   }
+  draw(g: CanvasRenderingContext2D) {
+    for (let y = 0; y < this.map.length; y++) {
+      for (let x = 0; x < this.map[y].length; x++) {
+        this.map[y][x].draw(g, x, y);
+      }
+    }
+  }
 }
 let map = new Map();
 function assertExhausted(x: never): never {
@@ -351,11 +358,7 @@ function draw(map: Map, player: Player) {
 }
 
 function drawMap(map: Map, g: CanvasRenderingContext2D) {
-  for (let y = 0; y < map.getMap().length; y++) {
-    for (let x = 0; x < map.getMap()[y].length; x++) {
-      map.getMap()[y][x].draw(g, x, y);
-    }
-  }
+  map.draw(g);
 }
 
 function gameLoop(map: Map) {
