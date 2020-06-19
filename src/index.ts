8c31947c998f8a8ee4afa8a9836390f12310cafd
wil pannell <wil.pannell@gmail.com>
2020-06-19 11:01:09 -0700

    run with parcel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 const TILE_SIZE = 30;
 const FPS = 30;
 const SLEEP = 1000 / FPS;
@@ -8,14 +7,21 @@ enum Tile {
   FLUX,
   UNBREAKABLE,
   PLAYER,
-  STONE, FALLING_STONE,
-  BOX, FALLING_BOX,
-  KEY1, LOCK1,
-  KEY2, LOCK2
+  STONE,
+  FALLING_STONE,
+  BOX,
+  FALLING_BOX,
+  KEY1,
+  LOCK1,
+  KEY2,
+  LOCK2
 }
 
 enum Input {
-  UP, DOWN, LEFT, RIGHT
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT
 }
 
 let playerx = 1;
@@ -26,7 +32,7 @@ let map: Tile[][] = [
   [2, 4, 2, 6, 1, 2, 0, 2],
   [2, 8, 4, 1, 1, 2, 0, 2],
   [2, 4, 1, 1, 1, 9, 0, 2],
-  [2, 2, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 2, 2]
 ];
 
 let inputs: Input[] = [];
@@ -49,13 +55,17 @@ function moveToTile(newx: number, newy: number) {
 }
 
 function moveHorizontal(dx: number) {
-  if (map[playery][playerx + dx] === Tile.FLUX
-    || map[playery][playerx + dx] === Tile.AIR) {
+  if (
+    map[playery][playerx + dx] === Tile.FLUX ||
+    map[playery][playerx + dx] === Tile.AIR
+  ) {
     moveToTile(playerx + dx, playery);
-  } else if ((map[playery][playerx + dx] === Tile.STONE
-    || map[playery][playerx + dx] === Tile.BOX)
-    && map[playery][playerx + dx + dx] === Tile.AIR
-    && map[playery + 1][playerx + dx] !== Tile.AIR) {
+  } else if (
+    (map[playery][playerx + dx] === Tile.STONE ||
+      map[playery][playerx + dx] === Tile.BOX) &&
+    map[playery][playerx + dx + dx] === Tile.AIR &&
+    map[playery + 1][playerx + dx] !== Tile.AIR
+  ) {
     map[playery][playerx + dx + dx] = map[playery][playerx + dx];
     moveToTile(playerx + dx, playery);
   } else if (map[playery][playerx + dx] === Tile.KEY1) {
@@ -68,8 +78,10 @@ function moveHorizontal(dx: number) {
 }
 
 function moveVertical(dy: number) {
-  if (map[playery + dy][playerx] === Tile.FLUX
-    || map[playery + dy][playerx] === Tile.AIR) {
+  if (
+    map[playery + dy][playerx] === Tile.FLUX ||
+    map[playery + dy][playerx] === Tile.AIR
+  ) {
     moveToTile(playerx, playery + dy);
   } else if (map[playery + dy][playerx] === Tile.KEY1) {
     remove(Tile.LOCK1);
@@ -83,24 +95,24 @@ function moveVertical(dy: number) {
 function update() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    if (current === Input.LEFT) moveHorizontal(-1);
+    else if (current === Input.RIGHT) moveHorizontal(1);
+    else if (current === Input.UP) moveVertical(-1);
+    else if (current === Input.DOWN) moveVertical(1);
   }
 
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        && map[y + 1][x] === Tile.AIR) {
+      if (
+        (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) &&
+        map[y + 1][x] === Tile.AIR
+      ) {
         map[y + 1][x] = Tile.FALLING_STONE;
         map[y][x] = Tile.AIR;
-      } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        && map[y + 1][x] === Tile.AIR) {
+      } else if (
+        (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) &&
+        map[y + 1][x] === Tile.AIR
+      ) {
         map[y + 1][x] = Tile.FALLING_BOX;
         map[y][x] = Tile.AIR;
       } else if (map[y][x] === Tile.FALLING_STONE) {
@@ -121,10 +133,8 @@ function draw() {
   // Draw map
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX)
-        g.fillStyle = "#ccffcc";
-      else if (map[y][x] === Tile.UNBREAKABLE)
-        g.fillStyle = "#999999";
+      if (map[y][x] === Tile.FLUX) g.fillStyle = "#ccffcc";
+      else if (map[y][x] === Tile.UNBREAKABLE) g.fillStyle = "#999999";
       else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
         g.fillStyle = "#0000cc";
       else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
@@ -156,7 +166,7 @@ function gameLoop() {
 
 window.onload = () => {
   gameLoop();
-}
+};
 
 const LEFT_KEY = 37;
 const UP_KEY = 38;
@@ -168,4 +178,3 @@ window.addEventListener("keydown", e => {
   else if (e.keyCode === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
   else if (e.keyCode === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
 });
-
