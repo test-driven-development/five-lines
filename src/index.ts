8f6726b1bb8e3245e0b38298793681bfda4577fd
wil pannell <wil.pannell@gmail.com>
2020-06-19 11:32:18 -0700

    introduce npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const TILE_SIZE = 30;
-const FPS = 30;
-const SLEEP = 1000 / FPS;
+const TILE_SIZE = 30
+const FPS = 30
+const SLEEP = 1000 / FPS
 
 enum Tile {
   AIR,
@@ -24,8 +24,8 @@ enum Input {
   RIGHT
 }
 
-let playerx = 1;
-let playery = 1;
+let playerx = 1
+let playery = 1
 let map: Tile[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
@@ -33,25 +33,25 @@ let map: Tile[][] = [
   [2, 8, 4, 1, 1, 2, 0, 2],
   [2, 4, 1, 1, 1, 9, 0, 2],
   [2, 2, 2, 2, 2, 2, 2, 2]
-];
+]
 
-let inputs: Input[] = [];
+let inputs: Input[] = []
 
 function remove(tile: Tile) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
       if (map[y][x] === tile) {
-        map[y][x] = Tile.AIR;
+        map[y][x] = Tile.AIR
       }
     }
   }
 }
 
 function moveToTile(newx: number, newy: number) {
-  map[playery][playerx] = Tile.AIR;
-  map[newy][newx] = Tile.PLAYER;
-  playerx = newx;
-  playery = newy;
+  map[playery][playerx] = Tile.AIR
+  map[newy][newx] = Tile.PLAYER
+  playerx = newx
+  playery = newy
 }
 
 function moveHorizontal(dx: number) {
@@ -59,21 +59,21 @@ function moveHorizontal(dx: number) {
     map[playery][playerx + dx] === Tile.FLUX ||
     map[playery][playerx + dx] === Tile.AIR
   ) {
-    moveToTile(playerx + dx, playery);
+    moveToTile(playerx + dx, playery)
   } else if (
     (map[playery][playerx + dx] === Tile.STONE ||
       map[playery][playerx + dx] === Tile.BOX) &&
     map[playery][playerx + dx + dx] === Tile.AIR &&
     map[playery + 1][playerx + dx] !== Tile.AIR
   ) {
-    map[playery][playerx + dx + dx] = map[playery][playerx + dx];
-    moveToTile(playerx + dx, playery);
+    map[playery][playerx + dx + dx] = map[playery][playerx + dx]
+    moveToTile(playerx + dx, playery)
   } else if (map[playery][playerx + dx] === Tile.KEY1) {
-    remove(Tile.LOCK1);
-    moveToTile(playerx + dx, playery);
+    remove(Tile.LOCK1)
+    moveToTile(playerx + dx, playery)
   } else if (map[playery][playerx + dx] === Tile.KEY2) {
-    remove(Tile.LOCK2);
-    moveToTile(playerx + dx, playery);
+    remove(Tile.LOCK2)
+    moveToTile(playerx + dx, playery)
   }
 }
 
@@ -82,23 +82,23 @@ function moveVertical(dy: number) {
     map[playery + dy][playerx] === Tile.FLUX ||
     map[playery + dy][playerx] === Tile.AIR
   ) {
-    moveToTile(playerx, playery + dy);
+    moveToTile(playerx, playery + dy)
   } else if (map[playery + dy][playerx] === Tile.KEY1) {
-    remove(Tile.LOCK1);
-    moveToTile(playerx, playery + dy);
+    remove(Tile.LOCK1)
+    moveToTile(playerx, playery + dy)
   } else if (map[playery + dy][playerx] === Tile.KEY2) {
-    remove(Tile.LOCK2);
-    moveToTile(playerx, playery + dy);
+    remove(Tile.LOCK2)
+    moveToTile(playerx, playery + dy)
   }
 }
 
 function update() {
   while (inputs.length > 0) {
-    let current = inputs.pop();
-    if (current === Input.LEFT) moveHorizontal(-1);
-    else if (current === Input.RIGHT) moveHorizontal(1);
-    else if (current === Input.UP) moveVertical(-1);
-    else if (current === Input.DOWN) moveVertical(1);
+    let current = inputs.pop()
+    if (current === Input.LEFT) moveHorizontal(-1)
+    else if (current === Input.RIGHT) moveHorizontal(1)
+    else if (current === Input.UP) moveVertical(-1)
+    else if (current === Input.DOWN) moveVertical(1)
   }
 
   for (let y = map.length - 1; y >= 0; y--) {
@@ -107,74 +107,74 @@ function update() {
         (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) &&
         map[y + 1][x] === Tile.AIR
       ) {
-        map[y + 1][x] = Tile.FALLING_STONE;
-        map[y][x] = Tile.AIR;
+        map[y + 1][x] = Tile.FALLING_STONE
+        map[y][x] = Tile.AIR
       } else if (
         (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) &&
         map[y + 1][x] === Tile.AIR
       ) {
-        map[y + 1][x] = Tile.FALLING_BOX;
-        map[y][x] = Tile.AIR;
+        map[y + 1][x] = Tile.FALLING_BOX
+        map[y][x] = Tile.AIR
       } else if (map[y][x] === Tile.FALLING_STONE) {
-        map[y][x] = Tile.STONE;
+        map[y][x] = Tile.STONE
       } else if (map[y][x] === Tile.FALLING_BOX) {
-        map[y][x] = Tile.BOX;
+        map[y][x] = Tile.BOX
       }
     }
   }
 }
 
 function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
+  let canvas = document.getElementById('GameCanvas') as HTMLCanvasElement
+  let g = canvas.getContext('2d')
 
-  g.clearRect(0, 0, canvas.width, canvas.height);
+  g.clearRect(0, 0, canvas.width, canvas.height)
 
   // Draw map
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX) g.fillStyle = "#ccffcc";
-      else if (map[y][x] === Tile.UNBREAKABLE) g.fillStyle = "#999999";
+      if (map[y][x] === Tile.FLUX) g.fillStyle = '#ccffcc'
+      else if (map[y][x] === Tile.UNBREAKABLE) g.fillStyle = '#999999'
       else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        g.fillStyle = "#0000cc";
+        g.fillStyle = '#0000cc'
       else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        g.fillStyle = "#8b4513";
+        g.fillStyle = '#8b4513'
       else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
-        g.fillStyle = "#ffcc00";
+        g.fillStyle = '#ffcc00'
       else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
-        g.fillStyle = "#00ccff";
+        g.fillStyle = '#00ccff'
 
       if (map[y][x] !== Tile.AIR)
-        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
     }
   }
 
   // Draw player
-  g.fillStyle = "#ff0000";
-  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  g.fillStyle = '#ff0000'
+  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE)
 }
 
 function gameLoop() {
-  let before = Date.now();
-  update();
-  draw();
-  let after = Date.now();
-  let frameTime = after - before;
-  let sleep = SLEEP - frameTime;
-  setTimeout(gameLoop, sleep);
+  let before = Date.now()
+  update()
+  draw()
+  let after = Date.now()
+  let frameTime = after - before
+  let sleep = SLEEP - frameTime
+  setTimeout(gameLoop, sleep)
 }
 
 window.onload = () => {
-  gameLoop();
-};
+  gameLoop()
+}
 
-const LEFT_KEY = 37;
-const UP_KEY = 38;
-const RIGHT_KEY = 39;
-const DOWN_KEY = 40;
-window.addEventListener("keydown", e => {
-  if (e.keyCode === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
-  else if (e.keyCode === UP_KEY || e.key === "w") inputs.push(Input.UP);
-  else if (e.keyCode === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
-  else if (e.keyCode === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
-});
+const LEFT_KEY = 37
+const UP_KEY = 38
+const RIGHT_KEY = 39
+const DOWN_KEY = 40
+window.addEventListener('keydown', e => {
+  if (e.keyCode === LEFT_KEY || e.key === 'a') inputs.push(Input.LEFT)
+  else if (e.keyCode === UP_KEY || e.key === 'w') inputs.push(Input.UP)
+  else if (e.keyCode === RIGHT_KEY || e.key === 'd') inputs.push(Input.RIGHT)
+  else if (e.keyCode === DOWN_KEY || e.key === 's') inputs.push(Input.DOWN)
+})
